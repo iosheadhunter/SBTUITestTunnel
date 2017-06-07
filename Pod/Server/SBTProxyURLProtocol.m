e57c8683b5b040b6e7d5c96972be20ebe81c8600
Elijah Ponkratenko <eelsajahber@gmail.com>
2017-06-07 17:47:22 +0300

    Ignoring invalid certs

--- Pod/Server/SBTProxyURLProtocol.m
+++ Pod/Server/SBTProxyURLProtocol.m
@@ -556,6 +556,14 @@ typedef void(^SBTStubUpdateBlock)(NSURLRequest *request);
     completionHandler(mRequest);
 }
 
+- (void)URLSession:(NSURLSession *)session
+              task:(NSURLSessionTask *)task
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler {
+    NSURLCredential *credential = [[NSURLCredential alloc] initWithTrust:challenge.protectionSpace.serverTrust];
+    completionHandler(NSURLSessionAuthChallengeUseCredential,credential);
+}
+
 -(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler
 {
     if ([self rewriteRuleForCurrentRequest] != nil) {
